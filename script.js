8b210953a69a76e62266cccf1d54fb40c6e95265
Heitor Filgueiras <hsfilgueiras2@gmail.com>
2022-08-13 16:00:50 -0300

    javascriptBaseDone

---
+++ script.js
@@ -0,0 +1,43 @@
+function toggleSelected(product){
+    if(product.classList.contains("prato")){
+        let currentSelected = document.querySelector(".selectedPlate");
+        if (currentSelected != null)
+        {
+            currentSelected.classList.toggle("prato");
+            currentSelected.classList.toggle("selectedPlate");
+        }
+        product.classList.toggle("prato");
+        product.classList.toggle("selectedPlate");
+
+    }
+    else if (product.classList.contains("bebida"))
+    {
+        let currentSelected = document.querySelector(".selectedDrink");
+        if (currentSelected != null)
+        {
+            currentSelected.classList.toggle("bebida");
+            currentSelected.classList.toggle("selectedDrink");
+        }
+        product.classList.toggle("bebida");
+        product.classList.toggle("selectedDrink");
+    }
+    else{
+    let currentSelected = document.querySelector(".selectedDessert");
+    if (currentSelected != null)
+    {
+        currentSelected.classList.toggle("sobremesa");
+        currentSelected.classList.toggle("selectedDessert");
+    }
+    product.classList.toggle("sobremesa");
+    product.classList.toggle("selectedDessert");}
+    let checkPlate = document.querySelector(".selectedPlate");
+    let checkDrink = document.querySelector(".selectedDrink");
+    let checkDessert =  document.querySelector(".selectedDessert");
+    if (checkPlate != null & checkDrink != null & checkDessert != null){
+        let button = document.querySelector(".bottomButton");
+        let textButton = document.querySelector(".textButton");
+        button.classList.replace("bottomButton","activeButton");
+        textButton.innerHTML="Fechar pedido";
+    }
+
+}
